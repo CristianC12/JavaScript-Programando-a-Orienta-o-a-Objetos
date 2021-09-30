777d86897f85ca3ae0044e1ce38d40f3f02c7598
Cristian Carlos Martini <cristiamcarlosm@hotmail.com>
2021-09-30 16:02:32 -0300

    aprimorando e ddeixando código mais clean

--- ContaCorrente.js
+++ ContaCorrente.js
@@ -1,7 +1,33 @@
+import { Cliente } from "./Cliente.js";
+
 export class ContaCorrente{
     agencia;
+    _cliente;
+
+
+    set cliente(novoValor){
+        if(novoValor instanceof Cliente){
+            this._cliente = novoValor;
+        }
+    }
+
+    get cliente(){
+        return this._cliente;
+    }
+
+
      // #saldo =0 https://github.com/tc39/proposal-class-fields#private-fields
     _saldo = 0;
+
+    get saldo(){
+        return this._saldo;
+    }
+
+    constructor(cliente, agencia){
+        this.agencia = agencia;
+        this.cliente = cliente;
+
+    }
 
     sacar(valor){
         if(this._saldo >= valor){
@@ -16,5 +42,10 @@ export class ContaCorrente{
             return;
         } 
         this._saldo += valor;           
+    }
+
+    transferir(valor, conta){
+        const valorSacado = this.sacar(valor);
+        conta.depositar(valorSacado);
     }
 }
--- index.js
+++ index.js
@@ -1,23 +1,16 @@
 import {Cliente}from "./Cliente.js"
 import {ContaCorrente}from "./ContaCorrente.js"
 
-const cliente1 = new Cliente();
-cliente1.nome = "Ricardo";
-cliente1.cpf = 11122233309;
+const cliente1 = new Cliente("Ricardo", 11122233309);
 
-const cliente2 = new Cliente();
-cliente2.nome = "Alice";
-cliente2.cpf = 88822233309;
+const cliente2 = new Cliente("Alice", 88822233309);
 
+const contaCorrenteRicardo = new ContaCorrente(cliente1, 1001);
+contaCorrenteRicardo.depositar(500);
 
-const contaCorrenteRicardo = new ContaCorrente();
-contaCorrenteRicardo.agencia = 1001;
+const contaCorrente2 = new ContaCorrente(cliente2, 102);
 
-contaCorrenteRicardo.depositar(-100);
-contaCorrenteRicardo.depositar(100);
-contaCorrenteRicardo.depositar(100);
-
-const valorSacado = contaCorrenteRicardo.sacar(50);
-console.log(valorSacado);
+contaCorrenteRicardo.transferir(200, contaCorrente2)
 
 console.log(contaCorrenteRicardo);
+console.log(contaCorrente2);
