f226c43f9e6f0d032f38f9b441ff3b3bd0b74da9
Cristian Carlos Martini <cristiamcarlosm@hotmail.com>
2021-09-30 14:56:12 -0300

    Adicionando modulos

--- index.js
+++ index.js
@@ -1,32 +1,11 @@
-class Cliente{
-    nome;
-    cpf;
+import {Cliente}from "./Cliente.js"
+import {ContaCorrente}from "./ContaCorrente.js"
 
-}
-
-class ContaCorrente{
-    agencia;
-    // #saldo = 0 https://github.com/tc39/proposal-class-fields#private-fields
-    _saldo = 0;
-
-    sacar(valor){
-        if (this._saldo >= valor){
-             this._saldo -= valor;
-        }
-    }
-
-    depositar(valor){
-        if(valor > 0){
-            this._saldo += valor;
-        }
-    }
-}
-
-const cliente1 =new Cliente();
+const cliente1 = new Cliente();
 cliente1.nome = "Ricardo";
 cliente1.cpf = 11122233309;
 
-const cliente2 =new Cliente();
+const cliente2 = new Cliente();
 cliente2.nome = "Alice";
 cliente2.cpf = 88822233309;
 
@@ -34,8 +13,11 @@ cliente2.cpf = 88822233309;
 const contaCorrenteRicardo = new ContaCorrente();
 contaCorrenteRicardo.agencia = 1001;
 
+contaCorrenteRicardo.depositar(-100);
 contaCorrenteRicardo.depositar(100);
-contaCorrenteRicardo.sacar(50);
+contaCorrenteRicardo.depositar(100);
 
-console.log(contaCorrenteRicardo)
+const valorSacado = contaCorrenteRicardo.sacar(50);
+console.log(valorSacado);
 
+console.log(contaCorrenteRicardo);
